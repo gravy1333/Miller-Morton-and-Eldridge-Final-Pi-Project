78a25562443e1cc4114e1678d1a058dfc9a375ba
meldridge437 <matthew.eldridge437@gmail.com>
2018-04-14 17:31:50 -0500

    added randomDie Fn to Dice Rolls. Set up the majority of the GUI. still in the process of getting the Dice rolls Fn to work via the buttons on the window

--- dice_rolls.py
+++ dice_rolls.py
@@ -1,23 +1,22 @@
 from random import randint
 
-
 def _4sided_dice_roll():
     return randint (1,4)
+
 def _6sided_dice_roll():
     return randint (1,6)
+
 def _8sided_dice_roll():
     return randint (1,8)
+
 def _10sided_dice_roll():
     return randint (1,10)
+
 def _20sided_dice_roll():
     return randint (1,20)
-d4 = _4sided_dice_roll()
-d6 = _6sided_dice_roll()
-d8 = _8sided_dice_roll()
-d10 = _10sided_dice_roll()
-d20 = _20sided_dice_roll()
 
-
+def randomDie():
+    return randint(1, 6)
     
 
 
