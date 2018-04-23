cf5b74f846c7f20696ba851adf37c574b6f882d1
meldridge437 <matthew.eldridge437@gmail.com>
2018-04-23 10:35:36 -0500

    player life box is set up still figuring out how we are going to update the health

--- GUI.py
+++ GUI.py
@@ -13,16 +13,53 @@ class Screen(Frame):
         self.grid(sticky='news')
 
     def setGUI(self):
-        #upper part
-        self.PLH = Label(window, text='PLayer Health')\
-                     .grid(row=0, column=0, rowspan=3, columnspan=4, sticky='news')
+        #player health
+        self.PLH = Label(window, bg='white')
+        self.PLH.grid(row=0, column=0, rowspan=3, columnspan=4, sticky='news')
+        
+        self.PL1N = Label(self.PLH, text='player 1', bg='white')
+        self.PL1N.grid(row=0, column=0, sticky='news')
+        self.PL1L = Label(self.PLH, text='001/001', fg='green', bg='white')
+        self.PL1L.grid(row=0, column=1, sticky='news')
+
+        self.PL2N = Label(self.PLH, text='player 2', bg='white')
+        self.PL2N.grid(row=1, column=0, sticky='news')
+        self.PL1L = Label(self.PLH, text='001/001', fg='green', bg='white')
+        self.PL1L.grid(row=1, column=1, sticky='news')
+
+        self.PL3N = Label(self.PLH, text='player 3', bg='white')
+        self.PL3N.grid(row=2, column=0, sticky='news')
+        self.PL1L = Label(self.PLH, text='001/001', fg='green', bg='white')
+        self.PL1L.grid(row=2, column=1, sticky='news')
+
+        self.PL4N = Label(self.PLH, text='player 4', bg='white')
+        self.PL4N.grid(row=3, column=0, sticky='news')
+        self.PL1L = Label(self.PLH, text='001/001', fg='green', bg='white')
+        self.PL1L.grid(row=3, column=1, sticky='news')
+
+        self.PL5N = Label(self.PLH, text='player 5', bg='white')
+        self.PL5N.grid(row=4, column=0, sticky='news')
+        self.PL1L = Label(self.PLH, text='001/001', fg='green', bg='white')
+        self.PL1L.grid(row=4, column=1, sticky='news')
+
+        for r in range(5):
+            self.PLH.rowconfigure(r, weight=2)
+        self.PLH.columnconfigure(0, weight=2)
+        self.PLH.columnconfigure(1, weight=1)
+
+        #player actions
+        self.PLA = Label(window, text='Player actions')\
+                    .grid(row=4, column=0, rowspan=2, columnspan=4, sticky='news')
+
+        #monster stats
         self.stats = Label(window, text='Stats')\
                        .grid(row=0, column=4, rowspan=3, columnspan=4, sticky='news')
+
+        #monster actions
         self.MA = Label(window, text='Moster Actions')\
-                    .grid(row=4, column=0, rowspan=2, columnspan=4, sticky='news')
-        self.PLA = Label(window, text='Player actions')\
                      .grid(row=4, column=4, rowspan=2, columnspan=4, sticky='news')
-        
+
+        #input bar
         self.player_input = Entry(window, bg='white')
         self.player_input.bind('<Return>', self.text_process())
         self.player_input.grid(row=6, column=0, columnspan=8, sticky='we')
@@ -70,7 +107,7 @@ class Screen(Frame):
 
     def roll(self, val):
         roll = randint(1, val)
-        self.RDis['text'] = str(roll)
+        self.RDis['text'] = [str(roll), "0" + str(roll)][roll < 10]
         
         
     def randRoll(self):
