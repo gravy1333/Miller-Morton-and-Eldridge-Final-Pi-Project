c7f12ee29028082aa825d97f56d430cfa7f22fb2
gravy1333 <lrm1333@yahoo.com>
2018-05-05 15:11:01 -0500

    moved player class to LED file

--- GUI.py
+++ GUI.py
@@ -138,46 +138,7 @@ class Screen(Frame):
     def changePlayerHealth(player, val):
         if (player == 1):
             Screen.PL1H['text'] = ""
-        
 
-class Player():
-    def __init__(self, maxH):
-        #temp is here to fix player instanciation issue
-        self.tempH = maxH
-        self.currentH = maxH
-        self.maxH = maxH
-
-    @property
-    def maxH(self):
-        return self._maxH
-
-    @maxH.setter
-    def maxH(self, val):
-        if (val > 0):
-            self._maxH = val
-
-    @property
-    def currentH(self):
-        return self._currentH
-
-    @currentH.setter
-    def currentH(self, val):
-        if (self.tempH + val < 0):
-            self._currentH = 0
-            self.tempH = 0
-
-        elif (self.tempH + val >= self.maxH):
-            self._currentH = self.maxH
-            self.tempH = self.maxH
-        else:
-            self._currentH += val
-            self.tempH += val
-
-    def getHealthPercent(self):
-        return int((float(self.currentH) / self.maxH) * 100)
-
-    def __str__(self):
-        return "{} / {}".format(self.currentH, self.maxH)
 
 diceVals = [0, 4, 6, 8, 10, 12, 20]
 WIDTH = 800
